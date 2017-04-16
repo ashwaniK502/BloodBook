a46ab7ca8eca6c66971e834bb0fdb5126edbaabe
ashwaniK502 <ashwanik502@gmail.com>
2017-04-16 15:00:03 +0530

    Create loginPage.php

---
+++ loginPage.php
@@ -0,0 +1,282 @@
+<html>
+<head><title>Welcome to BloodBook</title>
+<STYLE>
+
+.heading{
+    font-family: monospace;
+    font-size:20px; 
+    color:rgba(255,255,255, 0.75);
+}
+
+.para{
+    font-family: monospace;
+    font-size:15px; 
+    color:rgba(255,255,255, 0.75);
+}
+
+.container{
+	width: 1015;
+	height: 50px;
+	margin-left:0px;
+	margin-top: 0px;
+
+}
+.upContainer{
+	width: 507;
+	height: 50px;
+	margin-left:0px;
+	margin-top: 0px;
+
+}
+
+.inContainer{
+    width: 507;
+    height: 50px;
+    margin-left: 508px;
+    margin-top: 0px;
+}
+
+.myField{
+     width:1015;
+	height:510px;
+	margin-left: 158px;
+	border-color: transparent;
+	background-color:rgba(0,0,0,.65);
+	color: black;              
+}
+
+.btn
+{
+	margin-top: 0px;	
+    width:170px;
+	height:40px; 
+	color:blue;
+	font-size:18px;
+	background-color:transparent;
+	border:transparent;
+	outline:none;
+	font-variant: small-caps;
+	margin-left:0px; 
+}
+
+.upField
+{
+	 width:1015;	
+	 margin-left: 158px;
+	 height: 100px;
+	 margin-top: 30px;
+	 background:rgba(255,255,255, 0.75);
+	border-color: transparent;
+}
+.middleField
+{
+	 width:1015;	
+	 margin-left: 158px;
+	 height: 80px;
+	 background:rgba(0,0,0,0.4);
+	border-color: transparent;
+}
+.topField
+{
+	 width:1015;
+	 height: 140px;
+	 margin-left: 158px;
+    background-color: rgba(142, 114, 114,0.85);
+}
+.btn:hover
+{
+	margin-top: 0px;	
+    width:170px;
+	height:40px; 
+    color:rgba(0,0,0,0.8);
+	font-size:18px;
+	background-color:transparent;
+	border:transparent;
+	outline:none;
+	font-variant: small-caps;
+	margin-left:0px; 
+}
+
+.btn:active
+{
+	margin-top: 0px;	
+    width:170px;
+	height:40px; 
+	color:rgba(0,0,0,0.8);
+	font-size:18px;
+	background-color:transparent;
+	border:transparent;
+	outline:none;
+	font-variant: small-caps;
+	margin-left:0px; 
+}
+
+.imageClass{
+   margin-top: 0px;
+   margin-left:0px;
+   width:400px;
+	height:100px;  
+}
+.y_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+}
+
+.w_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.h_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:0px;
+	font-variant: small-caps;   
+	}
+	
+.t_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+
+.formClass{
+	margin-top: 50px;
+	margin-left:120px;
+	width : 800px;
+	color:rgba(255,255,255, 0.75);
+	height: 100px;
+}
+
+.emailClass{
+	margin-top: 0px;
+	margin-left:0px;
+	width : 400px;
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	height: 40px;
+}
+
+.passwordClass{
+	margin-top: 0px;
+	margin-left: 430px;
+	width : 400px;
+    color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	height:40px;
+}
+
+.submitBtn{
+    margin-top: 50px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:400px;
+	font-variant: small-caps;   
+}
+
+.submitBtn:active{
+    margin-top: 50px;
+    width:200px;
+	height:40px; 
+	color:yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:400px;
+	font-variant: small-caps;  
+}
+
+.submitBtn:hover{
+    margin-top: 50px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:400px;
+	font-variant: small-caps;  	
+}
+
+</STYLE>
+</head>
+<body background="Tue2014_BackgroundBokeh_3.jpg">
+<fieldset class="topField">
+<marquee><h2> Welcome to BloodBook. 
+This is a platform for finding blood in radius of few kilometers...</h2></marquee>
+<div>
+<a href="index.html" style="float:left; margin-left:20px;"><button class="h_blood_btn" >Home</button></a>
+</div>
+<div>
+<a href="why_blood.html" style="float:left; margin-left:20px;"><button class="y_blood_btn" >Why Donate Blood?</button></a>
+</div>
+<div>
+<a href="who_needs_blood.html" style="float:left; margin-left:20px;"><button class="w_blood_btn" >Who Needs Blood?</button></a>
+</div>
+<div>
+<a href="tips.html" style="float:left; margin-left:20px;"><button class="t_blood_btn" >Tips On Donation</button></a>
+</div>
+</fieldset>
+<fieldset class="upField">
+<div  class="container">
+<div class="upContainer">
+<img class="imageClass" src="icon.jpg"></div>
+<div class="inContainer">
+<button class="btn">forgot passowrd?</button>
+<button class="btn">new to BloodBook?</button>
+</div>
+</div>
+</fieldset>
+<fieldset class="myField">
+<form>
+<table class="formClass">
+<tr>
+<td class= "emailClass">
+<label>E-mail </label>&nbsp; &nbsp;<input type="E-mail" name="email" style=" padding-top: 10px; margin-left: 20px;">
+</td>
+<td class="passwordClass">
+<label>Password</label><input type="password" name="password" style=" padding-top: 10px; margin-left: 20px;">
+</td>
+</tr>
+</table>
+<input type="submit" value="login" class="submitBtn">
+</form>
+</fieldset>
+</body>
+</html>
