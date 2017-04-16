3a2bd1eba5e40446cf8fba45ec1afb2565b5e560
ashwaniK502 <ashwanik502@gmail.com>
2017-04-16 15:00:46 +0530

    Create registerPage.php

---
+++ registerPage.php
@@ -0,0 +1,353 @@
+<html>
+<head><title>Welcome to BloodBook</title>
+<STYLE>
+
+.container{
+	width: 1015;
+	height: 50px;
+	margin-left:0px;
+	margin-top: 0px;
+
+}
+.upContainer{
+	width: 507;
+	height: 50px;
+	margin-left:0px;
+	margin-top: 0px;
+
+}
+
+.inContainer{
+    width: 507;
+    height: 50px;
+    margin-left: 508px;
+    margin-top: 0px;
+}
+
+.myField{
+    width:1015;
+	height:630;
+	margin-left: 158px;
+	margin-top:50px;
+	border-color: transparent;
+	background-color:rgba(0,0,0,.65);
+	color: black;              
+}
+
+
+.topField
+{
+	 width:1015;
+	 height: 140px;
+	 margin-left: 158px;
+    background-color: rgba(142, 114, 114,0.85);
+}
+
+.imageClass{
+   margin-top: 0px;
+   margin-left:0px;
+   width:400px;
+	height:100px;  
+}
+
+.y_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+}
+
+.w_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.h_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:0px;
+	font-variant: small-caps;   
+	}
+.t_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+
+.y_blood_btn{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color:rgba(255,255,255, 0.75);
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+}
+
+.w_blood_btn:hover{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.h_blood_btn:hover{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:0px;
+	font-variant: small-caps;   
+	}
+	
+.y_blood_btn:hover{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.t_blood_btn:hover{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.y_blood_btn:active{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.w_blood_btn:active{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.h_blood_btn:active{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:0px;
+	font-variant: small-caps;   
+	}
+	
+.t_blood_btn:active{
+    margin-top: 0px;
+    width:200px;
+	height:40px; 
+	color: yellow;
+	font-size:28px;
+	background-color: transparent;
+	border: transparent;
+	outline:none;
+	margin-left:20px;
+	font-variant: small-caps;   
+	}
+	
+.formClass{
+	
+    width:825;
+	height:630;
+	margin-left: 190px;
+	margin-top:0px;
+	border-color: transparent;
+	background-color:transparent;  
+	color:rgba(255,255,255, 0.75);
+	font-size:15px; 
+	padding-top: 0px;
+    padding-bottom: 0px;  
+	border-collapse: collapse;       
+}
+
+
+body{
+	overflow-x: hidden;	
+}
+
+.textvalue{
+	width:200px;
+	height:30px;
+	margin-left:15px;
+	margin-right: 10px;
+	margin-bottom: 0px;
+	border:2px solid #CC444B;
+	outline:none;
+	padding:0 0px;
+
+}
+
+[type=submit],[type=reset]
+{
+	margin:0 auto;
+	color:rgba(255,255,255, 0.75);
+	border:transparent;
+	background-color:transparent;
+	
+}
+
+[type=submit]:hover,[type=reset]:hover
+{
+	background-color:transparent;
+	border:2px solid white;
+    color:yellow;
+}
+		
+</STYLE>
+</head>
+
+<body background="Tue2014_BackgroundBokeh_3.jpg">
+
+<fieldset class="topField">
+<marquee><h2> Join Hands With BloodBook..</h2></marquee>
+<div>
+<a href="index.html" style="float:left; margin-left:20px;"><button class="h_blood_btn" >Home</button></a>
+</div>
+<div>
+<a href="why_blood.html" style="float:left; margin-left:20px;"><button class="y_blood_btn" >Why Donate Blood?</button></a>
+</div>
+<div>
+<a href="who_needs_blood.html" style="float:left; margin-left:20px;"><button class="w_blood_btn" >Who Needs Blood?</button></a>
+</div>
+<div>
+<a href="tips.html" style="float:left; margin-left:20px;"><button class="t_blood_btn" >Tips On Donation</button></a>
+</div>
+</fieldset>
+
+<fieldset class="myField">
+<form >
+
+<table class="formClass" border="0">
+
+<tr style="height:18px;">
+<td><label>Enter your full name</label></td>
+<td><input type="text" placeholder="Your name" name="uname" class="textvalue" autofocus required></td>
+</tr>
+
+<tr style="height:18px;">
+<td><label>Enter your Date of Birth</label></td>>
+<td><input type="date" name="dob" class="textvalue" required></td>
+</tr>
+
+<tr style="height:18px;"> 
+<span style="margin-right:100px; margin-bottom:5px;display:block;">
+<td><label>Gender</label>&nbsp;&nbsp;</td>
+<td><input type="radio" name="Gender" value="male">Male <input type="radio" name="Gender" value="Female">Female</td>
+</span>
+</tr>
+
+<tr style="height:18px;">
+<td><label >Enter your Blood Group</label></td>
+<td><select name="bloodgroups" class="textvalue">
+<option>A+</option>
+<option>A-</option>
+<option>B+</option>
+<option>B-</option>
+<option>AB+</option>
+<option>AB-</option>
+<option>O+</option>
+<option>O-</option>
+</select> </td>
+</tr>
+
+<tr style="height:18px;">
+<td><label>Enter your email</label></td>
+<td><input type="email" name="emailid" placeholder="Enter your e-mail id" class="textvalue" required ></td>
+</tr>
+
+<tr style="height:18px;">
+<td><LABEL>Enter a password</LABEL></td>
+<td><input type="password" name="password"  placeholder="Your Password" class="textvalue" required><br></td>
+</tr>
+
+<tr style="height:18px;">
+<td><label>Enter your contact number</label></td>
+<td><input type="number" name="contact" class="textvalue" required><br></td>
+</tr>
+
+<tr style="height:18px;">
+<td><label>Last Blood Donation</label></td>
+<td><input type="date" name="lastdonation" class="textvalue" ><br></td>
+</tr>
+
+<tr style="height:20px;">
+<td><input type="submit"></td>  <td><input type="reset"></td>
+</tr>
+</table>
+</form>
+</fieldset>
+</body>
+</html>
